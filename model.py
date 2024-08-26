3938c6616656dd9a21d5c06509fedac973c6f2d2
zzink731 <159188279+zzink731@users.noreply.github.com>
2024-08-26 12:01:04 -0600

    organizing data, function creation

--- model.py
+++ model.py
@@ -2,13 +2,28 @@
 
 from opendssdirect import dss
 
+def load_dict_from_profiles(timestep):
+    
+    loads = {}
+    return loads
+
+def set_loads(loads : dict[str,float]):
+    for load in loads:
+        dss.Commands(f"Edit Load.{load} kw = {loads[load]} Pf = 0.9")
+
+def compute_results():
+    results = dss.Circuit.AllBusVMag()
+    return results
 
 
-
-dss.Command('Redirect "base_circuit.dss"')
-dss.Solution.Solve()
+dss.Commands('Redirect "base_circuit.dss"')
 
 
-print(dss.Circuit.AllBusNames())
-print(dss.Loads.AllNames())
-print(dss.Lines.AllNames())
+timesteps = [ts for ts in range(0,24)]
+
+for ts in timesteps:
+    loads = load_dict_from_profiles(ts)
+    set_loads(loads)
+    dss.Solution.Solve()
+
+    print(compute_results)
