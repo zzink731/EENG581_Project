bbea2759dfb27d61b15f7b8760ea570dfdec82c4
zzink731 <159188279+zzink731@users.noreply.github.com>
2024-08-26 15:24:29 -0600

    inclusion of loads in model

--- model.py
+++ model.py
@@ -1,10 +1,21 @@
 
 
 from opendssdirect import dss
+import pandas as pd
 
-def load_dict_from_profiles(timestep):
-    
+def load_dict_from_profiles(timestep : int):
+
     loads = {}
+    cust_profiles = pd.read_csv("data/customer_profiles.csv")
+    load_profiles = pd.read_csv("data/load_profiles.csv")
+
+    for i in cust_profiles.index:
+        node = cust_profiles.loc[i]["Node"]
+        num_cust = cust_profiles.loc[i]["Num_Customers"]
+        profile = cust_profiles.loc[i]["Demand_Profile"]
+        
+        loads["load"+str(node)] = load_profiles.loc[timestep]["Profile "+str(profile)] * num_cust
+
     return loads
 
 def set_loads(loads : dict[str,float]):
@@ -12,18 +23,22 @@ def set_loads(loads : dict[str,float]):
         dss.Commands(f"Edit Load.{load} kw = {loads[load]} Pf = 0.9")
 
 def compute_results():
-    results = dss.Circuit.AllBusVMag()
-    return results
+    voltage_results = dss.Circuit.AllBusVMag()
+    return voltage_results
+
+
+
 
 
 dss.Commands('Redirect "base_circuit.dss"')
 
+for ts in range(0,24):
 
-timesteps = [ts for ts in range(0,24)]
-
-for ts in timesteps:
+    
     loads = load_dict_from_profiles(ts)
     set_loads(loads)
+
     dss.Solution.Solve()
 
-    print(compute_results)
+    voltages = compute_results()
+    print(voltages)
